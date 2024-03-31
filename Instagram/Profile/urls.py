71482e37237a81444a7493b20d9cddbb8865dea4
yash-avasekar <stevefrayne3@gmail.com>
2024-04-01 00:35:25 +0530

    2nd commit : updated endpoints

--- Instagram/Profile/urls.py
+++ Instagram/Profile/urls.py
@@ -6,9 +6,13 @@ from . import views
 # URL ( endpoints ) goes here
 
 router = DefaultRouter()
-router.register("", views.ProfileViewsets)
+router.register("profile", views.ProfileViewsets)
+router.register("following", views.FollowingViewsets)
+router.register("follower", views.FollowerViewsets)
 
 urlpatterns = [
     path("register/user/", views.UserRegisterView.as_view()),
-    path("profile/", include(router.urls)),
+    path("user/login/", views.UserLoginView.as_view()),
+    path("user/logout/", views.UserLogoutView.as_view()),
+    path("user/", include(router.urls)),
 ]
