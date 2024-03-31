4f93573e23a0009557e82071afd16c45b4b1adef
yash-avasekar <stevefrayne3@gmail.com>
2024-03-31 22:43:25 +0530

    2nd commit : updated & added models

--- Instagram/Profile/models.py
+++ Instagram/Profile/models.py
@@ -19,25 +19,48 @@ class Profile(models.Model):
         website (str): The website URL of the user.
         created_at (DateTime): The date and time when the profile was created.
         updated_at (DateTime): The date and time when the profile was last updated.
-        id (UUID): The universally unique identifier for the profile.
+        id (UUID): The primary key and universally unique identifier for the profile.
 
     Methods:
         __str__(): Returns a string representation of the profile.
     """
 
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="Profile")
-    profile_picture = models.ImageField(upload_to="media/profile_images/")
+    profile_picture = models.ImageField(
+        upload_to="media/profile_images/", null=True, blank=True
+    )
     username = models.CharField(null=False, blank=False, unique=True, max_length=50)
     name = models.CharField(null=True, blank=True, max_length=100)
     bio = models.TextField(null=True, blank=True)
     website = models.URLField(null=True, blank=True, max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    id = models.UUIDField(
+        default=uuid.uuid4, editable=False, unique=True, primary_key=True
+    )
 
     def __str__(self):
         """
         String representation of the model instance.
         """
 
-        return f"{self.user}"
+        return self.username
+
+
+# Following Model
+class Following(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    following = models.OneToOneField(
+        Profile, on_delete=models.CASCADE, related_name="following_profile"
+    )
+
+
+# Follower Model
+class Follower(models.Model):
+    profile = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+    )
+    follower = models.OneToOneField(
+        Profile, on_delete=models.CASCADE, related_name="follower_profile"
+    )
